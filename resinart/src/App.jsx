d2dc853e7e049c69da8f1b1a41d36b447057a2ea
Victorsoares-vs <victortrexbk@gmail.com>
2025-10-16 19:14:51 -0300

    ...

--- resinart/src/App.jsx
+++ resinart/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./App.css"
 import imgResinart from "./assets/Resinart-removebg.png"
 
+
 function App() {
   return (
     <div className="container">
@@ -9,12 +10,7 @@ function App() {
         <img src= {imgResinart} alt="Banner da Resinart" />
       </div>
 
-      <div className="categorias">
-        <p className="categoria">Agendas</p>
-        <p className="categoria">Artigos</p>
-        <p className="categoria">Chaveiros</p>
-        <p className="categoria">Móveis</p>
-      </div>
+        
     </div>
   ) 
 }
---
+++ resinart/src/components/Categorias.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Categorias = () => {
+  return (
+    <div className="categorias">
+      <p className="categoria categoria--selecionada">Agendas</p>
+      <p className="categoria">Artigos</p>
+      <p className="categoria">Chaveiros</p>
+      <p className="categoria">Móveis</p>
+    </div>
+  )
+}
+
+export default Categorias
