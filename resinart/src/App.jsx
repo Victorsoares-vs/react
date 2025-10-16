bb38d6513d9666e0d4ea67db824d00934cbedc89
Victorsoares-vs <victortrexbk@gmail.com>
2025-10-16 18:17:00 -0300

    start

--- resinart/src/App.jsx
+++ resinart/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./App.css"
+import imgResinart from "./assets/Resinart-removebg.png"
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="container">
+      <div className='banner'>
+        <img src= {imgResinart} alt="Banner da Resinart" />
+      </div>
+
+      <div className="categorias">
+        <p className="categoria">Agendas</p>
+        <p className="categoria">Artigos</p>
+        <p className="categoria">Chaveiros</p>
+        <p className="categoria">Móveis</p>
+      </div>
+    </div>
+  ) 
 }
 
-export default App
+export default App 
