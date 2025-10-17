599788ce3caa13f011cb2460e7987cfff2ea03de
Victorsoares-vs <victortrexbk@gmail.com>
2025-10-17 00:34:47 -0300

    att

--- resinart/src/components/Card.jsx
+++ resinart/src/components/Card.jsx
@@ -1,18 +1,17 @@
-import imgAgenda from "../assets/agendas/agenda-escura.jpg"
-
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="card__textos">
-        <h2 className="card__titulo">Agenda Personalizada</h2>
-        <p>Agenda personalizada em resina, com Nome e flores, modelo resina escura</p>
-        <p className="card__preco">R$ 50,00</p>
+        <h2 className="card__titulo">{props.titulo}</h2>
+        <p>{props.descricao}a</p>
+        <p className="card__preco">{props.preco}</p>
       </div>
 
       <div className="card__img">
-        <img src={imgAgenda} alt="Foto do Produto" />
+        <img src={props.imagem} alt="Foto do Produto" />
       </div>
     </div>
+
   )
 }
 
--- resinart/src/components/Cards.jsx
+++ resinart/src/components/Cards.jsx
@@ -1,14 +1,26 @@
 import Card from './card'
+import imgAgenda from "../assets/agendas/agenda-escura.jpg"
 
 const Cards = () => {
   return (
     <div className="cards">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        titulo="Agenda Personalizada"
+        descricao=""
+        preco="R$ 50,00"
+        imagem={imgAgenda} />
+
+      <Card
+        titulo="Agenda Personalizada"
+        descricao="Agenda personalizada com nome, com capa em resina de cor escura."
+        preco="R$ 50,00"
+        imagem={imgAgenda} />
+
     </div>
+
+
+
+
   )
 }
 
