0df3c8d8d60245f35c149c2d7dba9bfd6829c5cb
Victorsoares-vs <victortrexbk@gmail.com>
2025-10-25 19:09:02 -0300

    atualização aula 02

--- react from scratch/src/index.jsx
+++ react from scratch/src/index.jsx
@@ -3,11 +3,20 @@ const root = createRoot(document.getElementById('root'))
 
 function Page() {
   return (
-    <ol>
-      <li>It's super fun!</li>
-      <li>It's new and exciting</li>
-      <li>I am liking so much.</li>
-    </ol>
+    <div>
+      <header>
+        <img src="src/assets/React-Logo-PNG.png" alt="Logo react" width= "40px" />
+      </header>
+      <main>
+        <h1>Reasons i'm excited to learn about React</h1>
+        <ol>
+          <li>It's super fun!</li>
+          <li>It's new and exciting</li>
+          <li>I am liking so much.</li>
+        </ol>
+      </main>
+      <footer>"© 2025 Soares development. All rights reserved."</footer>
+    </div>
   )
 }
 
