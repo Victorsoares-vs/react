c0f1999298c4e48ce8f0b2391d89956cfc380a22
Victorsoares-vs <victortrexbk@gmail.com>
2025-10-27 01:42:50 -0300

    challenge static pages - scrimba

---
+++ learn react code camp/aula 03/aula3/src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import Main from './Components/Main'
+import Navbar from './Components/Navbar'
+import './App.css'
+
+function App() {
+  return <div className="container">
+    <Main />
+    <Navbar />
+  </div>
+  
+}
+
+export default App
---
+++ learn react code camp/aula 03/aula3/src/Components/Main.jsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <h1>Main goes here</h1>
+}
+
+export default Main 
